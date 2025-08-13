323e154e27781ecbbdbfa41d6d23b6c885194e06
Mian Ameer Muhammad <mianameer830@gmail.com>
2025-08-14 00:12:53 +0500

    COMMIT DELETE

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.models.user import User
-from app.models.listing import Listing
-from app.models.favorite import Favorite
-from app.models.notification import Notification
-from app.models.message import Message
-from app.models.verification import Verification
-
-__all__ = ["User","Listing","Favorite","Notification","Message","Verification"]
